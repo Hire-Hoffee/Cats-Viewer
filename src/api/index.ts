7af63f68071308248e20593fd1ab96e8b557fd85
Rostislav Alekseev <mr.rostik.01@gmail.com>
2025-01-16 20:29:28 +0200

    .env load bug fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: "https://api.thecatapi.com",
   headers: {
     "Content-Type": "application/json",
-    "x-api-key": `${import.meta.env.API_KEY}`,
+    "x-api-key": `${import.meta.env.VITE_API_KEY}`,
   },
 });
