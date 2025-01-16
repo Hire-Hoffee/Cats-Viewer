62ff159c419226eb971a66f096a00f54708ad457
Rostislav Alekseev <mr.rostik.01@gmail.com>
2025-01-16 23:20:47 +0200

    deploy to gh pages revert

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import { store } from "./store/index.ts";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
